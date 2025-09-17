759453242d096bf465df84ada593c0731e84628f
Robby94gtr <robertogramazio94@gmail.com>
2025-09-17 13:37:24 +0200

    correzione

--- snack.js
+++ snack.js
@@ -21,7 +21,7 @@ const hamburger = {
 const secondBurger = {...hamburger};
 secondBurger.ingredients[0] = "Salad";
 ​
-console.log(hamburger.ingredients[0]); // Cheese
+console.log(hamburger.ingredients[0]); // Cheese , sbagliato è salad perché l'array non viene duplicato e quindi quando modifico dal secondBurger vado a cambiare anche l'array originale
 console.log(secondBurger.ingredients[0]); // Salad
 P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
 Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
@@ -47,7 +47,8 @@ const hamburger = {
 ​
 const secondBurger = structuredClone(hamburger);
 const thirdBurger = structuredClone(hamburger);
-Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3 oggetti */
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3 oggetti, sbagliato:
+già solo nel l'oggetto originale si nascondono altri due oggetti (maker e restaurant) quindi la risposta giusta era 9 oggetti (3x3) */
 
 
 
@@ -72,3 +73,68 @@ const restaurant = {
 };
 Qual è il metodo migliore per clonare l’oggetto chef, e perché? il metodo migliore è lo spread poichè l'oggetto chef contiene una funzione
 Qual è il metodo migliore per clonare l’oggetto restaurant, e perché? Il metodo migliore è structuredClone() poichè l'oggetto restaurant contiene oggetti complessi (data) */
+
+
+
+/* 🎯 Code Question 5 (Bonus)
+const hamburger = { 
+    name: "Cheese Burger", 
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+​
+const newRestaurant = {...hamburger.maker.restaurant};
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+const secondBurger = {...hamburger};
+secondBurger.maker.restaurant = newRestaurant;
+secondBurger.maker.name = "Chef Hyur";
+​
+console.log(hamburger.maker.name); // Chef Hyur
+console.log(secondBurger.maker.name); // Chef Hyur
+console.log(hamburger.maker.restaurant.name); // Hyur's Burgers, sbagliato è Hyur's II
+console.log(secondBurger.maker.restaurant.name); // Hyur's Burgers, sbagliato è Hyur's II 
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3 oggetti, anche qui sbagliato perchè già solo il primo oggetto ha tre oggetti, in tutto 5
+
+
+
+
+
+
+
+🎯 Code Question 6 (Bonus)
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+    restaurant: {
+        name: "Hyur's Burgers",
+        welcomeClient: () => {
+            console.log("Benvenuto!");
+        },
+        address: {
+            street: 'Main Street',
+            number: 123,
+            showAddress: () => {
+                console.log("Main Street 123");
+            }
+        },
+        isOpen: true,
+    }
+}
+Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+🎯 Snack  (Bonus)
+Crea una funzione che permette la copia profonda (deep copy) di un oggetto, che copia anche i suoi metodi (proprietà che contengono funzioni). Usa l’oggetto di Code Question 6 come test.
+
+⚠️ Serve usare una funzione ricorsiva! (fai un po’ di ricerca). */
